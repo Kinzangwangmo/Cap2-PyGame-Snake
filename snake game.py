765456973f76cae192f7141609f07b1e885fcd7a
kinzangwangmo <03230136.gcbs@rub.edu.bt>
2023-10-25 14:16:04 +0600

    first commit

---
+++ snake game.py
@@ -0,0 +1,21 @@
+import pygame,sys 
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((200,100))
+
+test_surface.fill(pygame.Color('red'))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill(pygame.Color('blue'))      
+    screen.blit(test_surface,(300, 250))        
+    pygame.display.update() 
+    clock.tick(50)  
+
+
+
+
