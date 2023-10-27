e7878c7f3299eb2597400f76a6bb0a5817278d44
kinzangwangmo <03230136.gcbs@rub.edu.bt>
2023-10-27 09:03:58 +0600

    add command

--- snake game.py
+++ snake game.py
@@ -1,22 +1,38 @@
-import pygame,sys 
+import pygame,sys,random 
+from pygame.math import Vector2
 
+class FRUIT:
+    def __init__(self):
+        self.x = 4
+        self.y = 3
+        self.pos = Vector2(self.x,self.y)
+ # create x and  y position  
+ 
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(200,150,90),fruit_rect)
+
+        
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+cell_size = 30
+cell_number = 20
+screen = pygame.display.set_mode((cell_size * cell_number,cell_number * cell_size))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((200,100))
 
-test_surface.fill(pygame.Color('red'))
+fruit = FRUIT()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    #  Adding in movement keys
-    # More code here
-    screen.fill(pygame.Color('blue'))      
-    screen.blit(test_surface,(300, 250))        
+    
+    #changed screen color to RGB
+    screen.fill((160,200,50)) 
+    fruit.draw_fruit()     
     pygame.display.update() 
-    clock.tick(50)  
+    clock.tick(50)
+
 
 
 
